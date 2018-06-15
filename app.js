9dffdce77a913c110e67dc878c7594c62fc336eb
Lucas Levin <lucaslevin1@gmail.com>
2018-06-15 12:08:58 -0400

    getting started with making tables in db

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-
+const { db } = require('./models');
 // const bodyParser = require('body-parser');
-
 const morgan = require('morgan');
-
 const app = express();
-
 const layout = require('./views/layout.js');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.get('/', (req, res, next) => {
   res.send(layout());
