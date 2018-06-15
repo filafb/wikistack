5e154cdeee7e7ed29d9eb14ea0a07390428617eb
Filadelfo Braz <filadelfo.braz@gmail.com>
2018-06-15 15:38:25 -0400

    up to the post action

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,15 +2,39 @@ const express = require('express');
 const router = express.Router();
 const addPageHTML = require('../views/addPage.js');
 const layout = require('../views/layout.js');
+const { db, Page, User } = require('../models/index.js');
 
 //sending default layout
 router.get('/', (req, res, next) => {
   res.send(layout());
 })
 
-router.post('/', (req, res, next) => {
-  res.json(req.body);
+router.post('/', async (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const title = req.body.title;
+  const content = req.body.content;
+  const status = req.body.status;
+  const slug = title.replace(" ", "-")
+
+  const page = new Page({
+    title: title,
+    content: content,
+    status: status,
+    slug: slug
+  })
+
+  try {
+    await page.save();
+    res.redirect('/');
+  }
+  catch(error) {
+    next(error)
+  }
+  // res.json(req.body); // to see how the body is returned
 })
+
+
 
 router.get('/add', (req, res, next) => {
   res.send(addPageHTML());
